217fcfdfaf249285cee8d570674c7244acb45169
Vishisht Priyadarshi <vishishtpriyadarshi867@gmail.com>
2022-01-25 06:02:09 +0000

    Add modules for transactions

---
+++ transaction_factory/transaction.py
@@ -0,0 +1,14 @@
+class Transaction:
+    """
+    This class models a transaction
+    """
+
+    def __init__(self, id, creation_time, value, reward):
+        self.id = id
+        self.creation_time = creation_time
+        self.miningTime = 0
+        self.value = value
+        self.reward = reward
+
+    def display(self):
+        print(self.id)
---
+++ transaction_factory/transaction_pool.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from network.broadcast import broadcast
+from transaction_factory.transaction import Transaction
+from utils import get_transmission_delay, PriorityQueue
+
+
+class TransactionPool:
+    """
+    Transaction pool for a full node
+    """
+
+    def __init__(self, env, id, neighbours_ids, nodes, params):
+        self.env = env
+        self.id = id
+        self.neighbours_ids = neighbours_ids
+        self.params = params
+        self.nodes = nodes
+        self.transaction_queue = PriorityQueue()
+        self.prev_transactions = []
+
+
+    def get_transaction(self, transaction_count):
+        """
+        Return transaction_count number of Transactions. 
+        Returns top transactions based on tx reward.
+        """
+        return self.transaction_queue.get(transaction_count)
+
+
+    def pop_transaction(self, transaction_count):
+        """
+        Remove transactions from transaction pool. 
+        Called when transactions are added by a received block or a block is mined.
+        """
+        popped_transactions = self.transaction_queue.pop(transaction_count)
+        self.prev_transactions.append(popped_transactions)
+
+
+    def put_transaction(self, transaction, source_location):
+        """
+        Add received transaction to the transaction pool and broadcast further
+        """
+        dest_location = self.nodes[self.id].location
+        delay = get_transmission_delay(source_location, dest_location)
+
+        # yield self.env.timeout(delay)
+        if (
+            not self.transaction_queue.is_present(transaction)
+            and transaction not in self.prev_transactions
+        ):
+            self.transaction_queue.insert(transaction)
+            # broadcast(
+            #     self.env,
+            #     transaction,
+            #     "Transaction",
+            #     self.id,
+            #     self.neighbourList,
+            #     self.params,
+            #     nodes=self.nodes,
+            # )
+            # if self.params["verbose"] == "vv":
+            #     print(
+            #         "%7.4f : %s accepted by %s"
+            #         % (self.env.now, transaction.id, self.id)
+            #     )
